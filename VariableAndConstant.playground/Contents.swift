a0252191d718b52663e8d7855f92931621999478
MasterUNG <phrombutr@gmail.com>
2017-10-10 08:05:41 +0700

    VariableAnConstant

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var nameString = "MasterUNg"
+nameString = nameString + " Dev iOS"
+print("Answer ==> \(nameString)")
+
+var surnameString = "Phrombutr"
+surnameString = "Doramon"
+
+
+
+
